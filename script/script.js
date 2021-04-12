ae418ca4a1d94302b2c4da4b7edbd9c58499380e
BFRIMPONG <bismark.frimpong@amalitech.org>
2021-04-12 15:45:30 +0000

    Stock bugs fixed

--- script/script.js
+++ script/script.js
@@ -16,6 +16,25 @@ class Product{
 
 class ShoppingCart {
     items = [];
+
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+    }
+
+    get totalAmount() {
+        const sum = this.items.reduce(
+            (preValue, curItem) => preValue + curItem.price,
+            0
+        );
+        return sum;
+    }
+
+    addProduct(product) {
+        const updatedItems = [...this.items];
+        updatedItems.push(product);
+        this.cartItems = updatedItems;
+    }
     
     render() {
         const cartEl = document.createElement('section');
@@ -24,6 +43,7 @@ class ShoppingCart {
             <button>Order Now</button>
         `;
         cartEl.className = 'cart'
+        this.totalOutput = cartEl.querySelector('h2')
         return cartEl;
     }
 }
@@ -34,8 +54,10 @@ class ProductItem{
     }
 
     addToCart () {
+        Products.addProductToCart(this.product);
         // console.log('Adding to cart...');
-        alert(JSON.stringify(this.product.title) + " added to cart");
+        // alert(JSON.stringify(this.product.title) + " added to cart");
+        // cart.addProduct();
     } 
     
     deleteItem () {
@@ -51,6 +73,7 @@ class ProductItem{
                 <div class = "product-items_content">
                     <h2>${this.product.title}</h2>
                     <h3>\$${this.product.price}</h3>
+                    <h3>${this.product.stock}</h3>
                     <p>${this.product.description}</p>
                     <button>Add to Cart</button>
                     <button id ="del">Delete</button>
@@ -74,35 +97,35 @@ class ProductList {
         new Product(
            'Mobile Phone',
            999.99,
-           'Avaliable',
+           'Stock: Avaliable',
            'An IPhone 12 Pro',
            'img/mobilephone.jpg',
         ),
         new Product(
             'Laptop',
             1320.00,
-            'Avaliable',
+            'Stock: Avaliable',
             'HP Pavilion x360 Laptop touch - 14-dw0097nr',
             'img/laptop.png',
          ),
         new Product(
             'Television',
             250.00,
-            'Avaliable',
+            'Stock: Avaliable',
             'TCL andriod TV S6500 series',
             'img/tv.jpg',
         ),
         new Product(
             'Projector',
             999.99,
-            'Avaliable',
+            'Stock: Avaliable',
             'An epson Home Cinema 2250 3LCD Full HD 1080p Projector',
             'img/projector.jpg',
         ),
         new Product(
             'Printer',
             129.99,
-            'Avaliable',
+            'Stock: Avaliable',
             'HP DeskJet 2320 All-in-One Printer',
             'img/printer.png',
          ),
@@ -125,8 +148,8 @@ class ProductList {
 class Shop {
     render () {
         const renderHook = document.getElementById('products');
-        const cart = new ShoppingCart();
-        const cartEl = cart.render();
+        this.cart = new ShoppingCart();
+        const cartEl = this.cart.render();
         const productList = new ProductList();
         // productList.render();
         const prodListEl = productList.render();
@@ -136,8 +159,24 @@ class Shop {
     }
 }
 
-const shop = new Shop();
-shop.render();
+class Products {
+    static cart;
+
+    static init() {
+        const shop = new Shop();
+        shop.render();
+        this.cart = shop.cart;
+        
+    }
+
+    static addProductToCart(product) {
+        this.cart.addProduct(product);
+    }
+}
+
+Products.init();
+
+
 
 const productList = new ProductList();
 productList.render();
