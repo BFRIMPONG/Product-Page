bdd139e549f7e09bbceef170d496edf4bb319897
BFRIMPONG <bismark.frimpong@amalitech.org>
2021-04-13 08:01:03 +0000

    inheritance inluded

--- script/script.js
+++ script/script.js
@@ -14,7 +14,34 @@ class Product{
     }
 }
 
-class ShoppingCart {
+class ElementAttribute {
+    constructor(attrName, attrValue) {
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component {
+    constructor(renderHookId) {
+      this.hookId = renderHookId;
+    }
+  
+    createRootElement(tag, cssClasses, attributes) {
+      const rootElement = document.createElement(tag);
+      if (cssClasses) {
+        rootElement.className = cssClasses;
+      }
+      if (attributes && attributes.length > 0) {
+        for (const attr of attributes) {
+          rootElement.setAttribute(attr.name, attr.value);
+        }
+      } 
+      document.getElementById(this.hookId).append(rootElement);
+      return rootElement;
+    }
+  }
+
+class ShoppingCart extends Component {
     items = [];
 
     set cartItems(value) {
@@ -30,6 +57,10 @@ class ShoppingCart {
         return sum;
     }
 
+    constructor(renderHookId) {
+        super(renderHookId);
+    }
+
     addProduct(product) {
         const updatedItems = [...this.items];
         updatedItems.push(product);
@@ -37,24 +68,23 @@ class ShoppingCart {
     }
     
     render() {
-        const cartEl = document.createElement('section');
+        const cartEl = this.createRootElement('section', 'cart');
         cartEl.innerHTML = `
-            <h2>Total: \$${0}</h2>
-            <button>Order Now</button>
+          <h2>Total: \$${0}</h2>
+          <button>Order Now!</button>
         `;
-        cartEl.className = 'cart'
-        this.totalOutput = cartEl.querySelector('h2')
-        return cartEl;
-    }
-}
+        this.totalOutput = cartEl.querySelector('h2');
+      }
+}  
 
-class ProductItem{
-    constructor(product){
-        this.product= product; 
+class ProductItem extends Component {
+    constructor(product, renderHookId){
+        super(renderHookId);
+        this.product = product; 
     }
 
     addToCart () {
-        Products.addProductToCart(this.product);
+        ProductPage.addProductToCart(this.product);
         // console.log('Adding to cart...');
         // alert(JSON.stringify(this.product.title) + " added to cart");
         // cart.addProduct();
@@ -65,8 +95,7 @@ class ProductItem{
     }
 
     render() {
-        const prodEl = document.createElement('li');
-        prodEl.className = 'product-item';
+        const prodEl = this.createRootElement ('li', 'product-item');
         prodEl.innerHTML = `
             <div>
                 <img src ="${this.product.imageUrl}" alt= "${this.product.title}">
@@ -85,14 +114,13 @@ class ProductItem{
         addCartButton.addEventListener('click', this.addToCart.bind(this))
         const deleteButton = prodEl.querySelector('#del');
         deleteButton.addEventListener('click', this.deleteItem.bind(this))
-        return prodEl;
-
     }
 }
 
 
 
-class ProductList {
+class ProductList extends Component {
+    
     products = [
         new Product(
            'Mobile Phone',
@@ -131,35 +159,30 @@ class ProductList {
          ),
     ]
 
-    constructor() {}
+    constructor(renderHookId) {
+        super(renderHookId);
+    }
     render() {
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
+        this.createRootElement('ul', 'product-list', [
+          new ElementAttribute('id', 'prod-list')
+        ]);
         for (const prod of this.products) {
-            const productItem = new ProductItem(prod)
-            const prodEl = productItem.render();
-            prodList.append(prodEl);
+          const productItem = new ProductItem(prod, 'prod-list');
+          productItem.render();
         }
-        return prodList;
-
-    }   
-}
-
-class Shop {
-    render () {
-        const renderHook = document.getElementById('products');
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
-        const productList = new ProductList();
-        // productList.render();
-        const prodListEl = productList.render();
-
-        renderHook.append(cartEl);
-        renderHook.append(prodListEl);
     }
 }
 
-class Products {
+class Shop {
+    render() {
+      this.cart = new ShoppingCart('productPage');
+      this.cart.render();
+      const productList = new ProductList('productPage');
+      productList.render();
+    }
+  }
+
+class ProductPage {
     static cart;
 
     static init() {
@@ -174,10 +197,10 @@ class Products {
     }
 }
 
-Products.init();
+ProductPage.init();
 
 
 
-const productList = new ProductList();
-productList.render();
+// const productList = new ProductList();
+// productList.render();
 
